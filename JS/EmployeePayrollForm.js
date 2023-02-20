bb510887d8825fbd5997379092f64de6a77c261e
Your Name <you@example.com>
2023-02-20 08:38:10 +0530

    Added: View employee payroll details in tabular format

---
+++ JS/EmployeePayrollForm.js
@@ -0,0 +1,108 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error')
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = '';
+            return;
+        }
+
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = '';
+        } catch (e) {
+            textError.textContent = e;
+        }
+    })
+
+    function getSalary() {
+        output.textContent = salary.value;
+    }
+
+    const salary = document.querySelector('#salary')
+    const output = document.querySelector('.salary-output')
+    output.textContent = salary.value
+    salary.addEventListener('input', getSalary)
+
+});
+
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
+    } catch (e) {
+        return;
+    }
+}
+
+const createAndUpdateStorage = function (employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if (employeePayrollList != undefined) {
+        employeePayrollList = employeePayrollList.push(employeePayrollData)
+    } else {
+        employeePayrollList = [employeePayrollData] //need to ask in doubt session
+    }
+    alert(employeePayrollList.toString())
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData(); //creation of object
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    } catch (e) {
+        setTextValue('.text-error', e)
+        throw e;
+    }
+
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop()
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop()
+    employeePayrollData.department = getSelectedValues('[name=department]')
+    employeePayrollData.salary = getInputValueById('#salary')
+    employeePayrollData.note = getInputValueById('#notes')
+    let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach((item) => {
+        if (item.checked) {
+            selItems.push(item.value)
+        }
+    })
+    return selItems;
+}
+
+const resetForm = () => {
+    setValue('#name',' ');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary',' ');
+    setValue('#notes',' ');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2020');
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allTime = document.querySelectorAll(propertyValue);
+    allTime.forEach(item=>{
+        item.checked = false
+    });
+}
+
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
